fe67569ab04cb1d3e3ed1104f0fdddff4d439339
Joseph Hicks <jhicksdev@gmail.com>
2023-04-26 18:27:02 -0400

    Provide TSV file and JSON generator

---
+++ to_json.py
@@ -0,0 +1,65 @@
+import json
+import pathlib
+
+DIRNAME = pathlib.Path(__file__).parent.resolve()
+
+rankings = None
+
+
+def isfloat(x):
+    try:
+        float(x)
+    except (TypeError, ValueError):
+        return False
+    else:
+        return True
+
+
+def isint(x):
+    try:
+        a = float(x)
+        b = int(a)
+    except (TypeError, ValueError):
+        return False
+    else:
+        return a == b
+
+
+def convert_to_number(x):
+    return int(x) if isint(x) else float(x) if isfloat(x) else x
+
+
+def get_ranking(title):
+    return list(filter(lambda x: x["title"] == title, rankings))[0]
+
+
+with open(f"{DIRNAME}/rankings.tsv") as file:
+    rows = list(map(lambda x: x.split("\t"), file.read().split("\n")[1:]))
+    rankings = list(
+        map(
+            lambda x: {
+                "title": x[0],
+                "version": convert_to_number(x[1]),
+                "records": [],
+            },
+            sorted(set(map(lambda x: (x[0], x[1]), rows))),
+        )
+    )
+    for row in rows:
+        title, _, country, rank, score = row
+        get_ranking(title)["records"].append(
+            {
+                "country": country,
+                "rank": convert_to_number(rank),
+                "score": convert_to_number(score),
+            }
+        )
+    file.close()
+
+with open(f"{DIRNAME}/rankings.json", "w") as file:
+    json.dump(rankings, file, ensure_ascii=False, indent=2)
+    file.close()
+
+with open(f"{DIRNAME}/rankings.min.json", "w") as file:
+    json.dump(rankings, file, separators=(",", ":"), ensure_ascii=False)
+    file.close()
